2a2f0edaf3743877ff80166a51062b09e51703e0
Haolun Zhang <chshawkn@users.noreply.github.com>
2020-01-15 10:28:04 +1300

    env:ELASTICSEARCH_HOSTS

--- docker-compose_up.ps1
+++ docker-compose_up.ps1
@@ -4,7 +4,7 @@ $env:PWD = ($p.substring(0,2).ToLower() + $p.substring(2)).replace("\", "/")
 Get-ChildItem Env:PWD
 
 $env:IMAGE_ARG_KIBANA_IMAGE_NAME = 'kibana'
-#$env:ELASTICSEARCH_HOSTS = 'http://172.16.238.62:9200'
+$env:ELASTICSEARCH_HOSTS = 'http://172.16.238.62:9200'
 
 docker-compose down -v
 docker-compose up -d
